b58a4297545b79c3bab55675fc3667b169b2374a
dya_dya <140974835+defl-orator@users.noreply.github.com>
2026-03-25 19:59:57 +0500

    Add files via upload

--- etis-test.user.js
+++ etis-test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ЕТИС REBORN
 // @namespace    http://tampermonkey.net/
-// @version      1.8103
+// @version      1.8104
 // @changelog    Настройка внешнего вида. Удобная установка для пользователей iOS
 // @description  Глобальный редизайн ЕТИСа
 // @author       dya_dya
